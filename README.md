6c6c432c0e73d2c20a1ca056326c43b8f5f7ff5f
Sampeo <timon.schad@edu.fhdw.de>
2024-01-17 18:50:45 +0100

    documentary for git hooks

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # aiwars
 
 Project for the "Technologie-Trends" course of the Wirtschaftsinformatik master 2023 at the FHDW Mettmann
+
+## Project Setup
+
+The init-script.sh or init-script.bat script should be executed when the project is initially set up.
+It copies the pre-commit hook into the hooks folder. 
+The pre-commit hook protects you from checking in faulty/non-executable code into all branches of the repository if the configured automated tests fail.
--- init-script.bat
+++ init-script.bat
@@ -1,6 +1,6 @@
-:: The init-script.sh script should be executed when the project is initially set up.
-:: It copies the pre-commit hooks into the hooks folder.
-:: The pre-commit hook protects you from checking in faulty/non-executable code into
-:: all branches of the repository.
+:: The init-script.sh or init-script.bat script should be executed when the project is initially set up.
+:: It copies the pre-commit hook into the hooks folder.
+:: The pre-commit hook protects you from checking in faulty/non-executable code into all branches of the repository
+:: if the configured automated tests fail.
 
 xcopy "hooks\pre-commit" ".git\hooks" /Y
